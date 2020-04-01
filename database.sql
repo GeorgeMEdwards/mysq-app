0bc1cae34e08336ee8b60c80e679bd5922444489
GeorgeMEdwards <georgemwangi96@yahoo.com>
2020-04-01 08:16:32 -0500

    initial commit

---
+++ database.sql
@@ -0,0 +1,9 @@
+CREATE DATABASE IF NOT EXISTS customer;
+USE customer;
+CREATE TABLE users ( 
+	id int AUTO_INCREMENT,
+	username varchar(20),
+	fullname varchar(20),
+	password varchar(128),
+	PRIMARY KEY (id)
+);
